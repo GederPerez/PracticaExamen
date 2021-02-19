11575d84a30c4e1d7504c0ba2b7a8d2ff581e28f
lanz <lanz@MMLMICCCD7C88F>
2021-02-19 15:54:20 -0600

    Trabajo Git

---
+++ Git609993.java/src/git609993/java/Git609993Java.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git609993.java;
+
+/**
+ *
+ * @author lanz
+ */
+public class Git609993Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String nombre= "Geder Pérez";
+        String matricula= "60993";
+        System.out.println(" Soy " + nombre + " Mi matricula es " + matricula);
+    }
+    
+}
